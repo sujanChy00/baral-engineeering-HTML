dc2f64d4a5a17fbf8a0472a4ee26fcbb14668067
sujanChy00 <suzu9050@gmail.com>
2025-03-29 01:58:36 +0545

    Add sidebar functionality and animation effects; implement Embla carousel

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ AOS.init({
   once: true,
 });
 
+const emblaNode = document.querySelector(".embla");
+const options = { loop: false };
+const plugins = [EmblaCarouselAutoplay()];
+const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+
+const prevBtn = document.querySelector(".embla__prev");
+const nextBtn = document.querySelector(".embla__next");
+
+if (prevBtn && nextBtn) {
+  prevBtn.addEventListener("click", () => emblaApi.scrollPrev());
+  nextBtn.addEventListener("click", () => emblaApi.scrollNext());
+
+  const updatePrevNextBtns = () => {
+    if (emblaApi.canScrollPrev()) {
+      prevBtn.removeAttribute("disabled");
+    } else {
+      prevBtn.setAttribute("disabled", "disabled");
+    }
+
+    if (emblaApi.canScrollNext()) {
+      nextBtn.removeAttribute("disabled");
+    } else {
+      nextBtn.setAttribute("disabled", "disabled");
+    }
+  };
+
+  emblaApi
+    .on("select", updatePrevNextBtns)
+    .on("init", updatePrevNextBtns)
+    .on("reInit", updatePrevNextBtns);
+}
+
 function setActiveLink() {
   const currentPath = window.location.pathname;
   const nav = document.getElementById("nav");
@@ -26,3 +58,23 @@ if (footerText) {
 }
 
 setActiveLink();
+
+const openSidebar = document.getElementById("openSidebar");
+const closeSidebar = document.getElementById("closeSidebar");
+const sidebar = document.querySelector(".sidebar");
+const closeSidebarButton = document.getElementById("closeSidebarButton");
+
+openSidebar.addEventListener("click", () => {
+  sidebar.classList.remove("hidden");
+  document.body.style.overflow = "hidden";
+});
+
+closeSidebar.addEventListener("click", () => {
+  sidebar.classList.add("hidden");
+  document.body.style.overflow = "auto";
+});
+
+closeSidebarButton.addEventListener("click", () => {
+  sidebar.classList.add("hidden");
+  document.body.style.overflow = "auto";
+});
